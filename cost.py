7801f77c259a55aba9f9d38d665eb7f9d6893070
Isabella Howell <isabellahowell@gmail.com>
2019-07-23 19:29:28 +0100

    UPDATED

---
+++ cost.py
@@ -0,0 +1,21 @@
+
+
+def hotel_choice(number):
+    if number 1:
+        return 100
+    if number 2:
+        return 150
+    if number 3: 200
+
+
+def staying_how_long(days):
+    if days < 3:
+        return 40 * days
+    elif days >= 3 and days < 7:
+        return days * 40 - 20
+    elif days >= 7:
+        return days * 40 - 50
+
+    def trip_cost(city,days):
+        totalcost= hotel_choice(number)+staying_how_long(days)
+        return totalcost
